9b26b525d1fcf5912b9f536423e0d5d6dd63ca36
Tadas Kundelis <kundelist@gmail.com>
2022-12-08 10:53:47 +0200

    days 5, 6, 7

---
+++ day5/solution.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+
+function processInput() {
+    const rawData = fs.readFileSync('./day5/input.txt', 'utf8')
+    return rawData.split('\n')
+        .map(line => line.match(/\d+/g).map(Number))
+        .map(([count, from, to]) => [count, from - 1, to - 1])
+}
+
+const commands = processInput()
+
+const stacks = [
+    ['N', 'S', 'D', 'C', 'V', 'Q', 'T'],
+    ['M', 'F', 'V'],
+    ['F', 'Q', 'W', 'D', 'P', 'N', 'H', 'M'],
+    ['D', 'Q', 'R', 'T', 'F'],
+    ['R', 'F', 'M', 'N', 'Q', 'H', 'V', 'B'],
+    ['C', 'F', 'G', 'N', 'P', 'W', 'Q'],
+    ['W', 'F', 'R', 'L', 'C', 'T'],
+    ['T', 'Z', 'N', 'S'],
+    ['M', 'S', 'D', 'J', 'R', 'Q', 'H', 'N'],
+]
+
+function getResponse(stacks) {
+    return stacks.map(s => s.pop()).join('')
+}
+
+function solvePart1() {
+    const _stacks = stacks.map(s => s.slice())
+    for (const [count, from, to] of commands) {
+        for (let i = count; i > 0; i--) {
+            _stacks[to].push(_stacks[from].pop())
+        }
+    }
+    return getResponse(_stacks)
+}
+
+function solvePart2() {
+    const _stacks = stacks.map(s => s.slice())
+    for (const [count, from, to] of commands) {
+        const fromStack = _stacks[from]
+        const toStack = _stacks[to]
+        _stacks[to] = toStack.concat(fromStack.slice(-count))
+        _stacks[from] = fromStack.slice(0, fromStack.length - count)
+    }
+    return getResponse(_stacks)
+}
+
+console.log(solvePart1() == 'FRDSQRRCD')
+console.log(solvePart2() == 'HRFTQVWNN')
---
+++ day7/solution.js
@@ -0,0 +1,124 @@
+const fs = require('fs')
+
+function readInput() {
+    return fs.readFileSync('./day7/input.txt', 'utf8').split('\n')
+}
+
+class File {
+    constructor(name, size) {
+        this.name = name
+        this.size = size
+    }
+}
+
+class Directory {
+    constructor(parentDir) {
+        this.parentDir = parentDir
+        this.files = []
+        this.childDirs = {}
+    }
+
+    addChildDirIfNotExists(dirName) {
+        this.childDirs[dirName] = this.childDirs[dirName] || new Directory(this)
+    }
+
+    addFile(file) {
+        this.files.push(file)
+    }
+
+    computeDirSize() {
+        const fileSizes = this.files.map(file => file.size)
+        const childDirSizes = Object.values(this.childDirs).map(dir => dir.computeDirSize())
+        return fileSizes.reduce((a, b) => a + b, 0) + childDirSizes.reduce((a, b) => a + b, 0)
+    }
+}
+
+class FileSystem {
+    constructor() {
+        this.root = new Directory(null)
+        this.currentDir = this.root
+    }
+
+    addDir(dirName) {
+        this.currentDir.addChildDirIfNotExists(dirName)
+    }
+
+    addFile(file) {
+        this.currentDir.addFile(file)
+    }
+
+    goToDir(dir) {
+        if (dir === '/') {
+            this.currentDir = this.root
+        } else if (dir === '..') {
+            this.currentDir = this.currentDir.parentDir
+        } else {
+            this.currentDir = this.currentDir.childDirs[dir]
+        }
+    }
+}
+
+function processCommands() {
+    let i = 0
+
+    while (i < input.length) {
+        const line = input[i]
+        const [_, command, dir] = line.split(' ')
+
+        switch (command) {
+            case 'cd':
+                fileSystem.goToDir(dir)
+                break
+            case 'ls':
+                while (i + 1 < input.length && input[i + 1][0] !== '$') addDirOrFile(input[++i])
+                break
+        }
+        i++
+    }
+}
+
+function addDirOrFile(line) {
+    const [a, b] = line.split(' ')
+    if (a === 'dir') {
+        const dirName = b
+        fileSystem.addDir(dirName)
+    } else {
+        const [size, name] = [a, b]
+        fileSystem.addFile(new File(name, Number(size)))
+    }
+}
+
+function traverse_and_sum(dir) {
+    const size = dir.computeDirSize()
+    const total = size <= 100000 ? size : 0
+    return Object.values(dir.childDirs).reduce((acc, curr) => acc + traverse_and_sum(curr), total)
+}
+
+const input = readInput()
+const fileSystem = new FileSystem()
+
+function solvePart1() {
+    processCommands()
+    return traverse_and_sum(fileSystem.root)
+}
+
+const TOTAL_AVAILABLE_SPACE = 70000000
+const NEEDED_FREE_SPACE = 30000000
+
+function findSmallestDir(dir, missing) {
+    const currentSize = dir.computeDirSize()
+    const childDirSizes = Object.values(dir.childDirs).map(childDir => findSmallestDir(childDir, missing))
+    return Math.min(...[currentSize].concat(childDirSizes).filter(size => size >= missing))
+}
+
+function solvePart2() {
+    const totalUsedSpace = fileSystem.root.computeDirSize()
+    const currentFreeSpace = TOTAL_AVAILABLE_SPACE - totalUsedSpace
+    const missingSpace = NEEDED_FREE_SPACE - currentFreeSpace
+    return findSmallestDir(fileSystem.root, missingSpace)
+}
+
+console.log(solvePart1() == 1583951)
+console.log(solvePart2() == 214171)
+
+
