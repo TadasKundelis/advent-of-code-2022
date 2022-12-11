a70801d0b361f566b48131cda21c88d400799b33
Tadas Kundelis <kundelist@gmail.com>
2022-12-12 00:04:31 +0200

    scala day 3 refactor

--- 03/solution.scala
+++ 03/solution.scala
@@ -3,33 +3,22 @@ import scala.io.Source
 object Solution extends App {
   val lines = Source.fromFile("./input.txt").getLines.toList
   val lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
-  val letters = s" ${lowerCaseLetters}${lowerCaseLetters.toUpperCase}"
+  val letters = s" $lowerCaseLetters${lowerCaseLetters.toUpperCase}"
 
   println(solvePart1(lines) == 7875)
   println(solvePart2(lines) == 2479)
 
-  def findCommonCharPart1(line: String): Char = {
-    val mid = line.length / 2
-    val (firstHalf, secondHalf) = (line.slice(0, mid), line.slice(mid, mid * 2))
-    firstHalf.toSet.intersect(secondHalf.toSet).head
+  def splitLine(line: String): List[String] = {
+    List(line.slice(0, line.length / 2), line.slice(line.length / 2, line.length))
   }
 
-  def solvePart1(lines: List[String]): Int = {
-    lines
-      .map(findCommonCharPart1)
-      .map(c => letters.indexOf(c))
-      .sum
+  def findCommonChar(lists: List[String]): Char = lists.map(_.toSet).reduce(_ intersect _).head
+
+  def countSumOfCommonChars(lists: List[List[String]]): Int = {
+    lists.map(findCommonChar).map(c => letters.indexOf(c)).sum
   }
 
-  def findCommonCharPart2(lines: List[String]): Char = {
-    lines.map(_.toSet).reduce(_ intersect _).head
-  }
+  def solvePart1(lines: List[String]): Int = countSumOfCommonChars(lines.map(splitLine))
 
-  def solvePart2(lines: List[String]): Int = {
-    lines
-      .grouped(3)
-      .map(findCommonCharPart2)
-      .map(c => letters.indexOf(c))
-      .sum
-  }
+  def solvePart2(lines: List[String]): Int = countSumOfCommonChars(lines.grouped(3).toList)
 }
