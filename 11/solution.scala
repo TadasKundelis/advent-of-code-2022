b70baaf9e0c74fc11232c46a8294c583399dd316
Tadas Kundelis <kundelist@gmail.com>
2022-12-11 17:43:07 +0200

    refactor

--- 11/solution.scala
+++ 11/solution.scala
@@ -1,8 +1,13 @@
 import scala.io.Source
 import scala.math._
+import scala.util.matching.Regex.Match
 
 case class Monkey(
-                   number: BigInt, items: List[BigInt], operation: (BigInt) => BigInt, resolveNextMonkey: (BigInt) => BigInt, inspectedCount: BigInt = 0) {
+  number: Int, 
+  items: List[Int], 
+  operation: (BigInt) => BigInt, 
+  resolveNextMonkey: (Int) => Int,
+  inspectedCount: BigInt = 0) {
 
   def incrementInspectedCount(): Monkey = {
     this.copy(inspectedCount = this.inspectedCount + 1)
@@ -10,39 +15,39 @@ case class Monkey(
 }
 
 object Monkey {
-  def resolveOperation(input: String): Option[BigInt] => BigInt => BigInt = {
-    def sum(a: Option[BigInt])(b: BigInt): BigInt = a.getOrElse(b) + b
-
-    def subtract(a: Option[BigInt])(b: BigInt): BigInt = (a.getOrElse(b) - b)
-
-    def multiply(a: Option[BigInt])(b: BigInt): BigInt = (a.getOrElse(b) * b) % 9699690 // remove hardcoded value
-
-    val operations = Map(
-      "+" -> sum,
-      "-" -> subtract,
-      "*" -> multiply,
-    )
-
-    operations(input)
+  def resolveOperation(regexMatch: Match): (Option[BigInt], BigInt) => BigInt = regexMatch.group(3).mkString match {
+    case v if v == "+" => (a: Option[BigInt], b: BigInt) => a.getOrElse(b) + b
+    case v if v == "*" => (a: Option[BigInt], b: BigInt) => (a.getOrElse(b) * b) % 9699690
   }
+
+  def createNextMonkeyResolver(regexMatch: Match): (Int) => Int = {
+    val divider = regexMatch.group(5).toInt
+    val destinationIfTrue = regexMatch.group(6).toInt
+    val destinationIfFalse = regexMatch.group(7).toInt 
+    (num: Int) => if (num % divider == 0) destinationIfTrue else destinationIfFalse
+  }
+
+  def resolveItems(regexMatch: Match): List[Int] = {
+    regexMatch.group(2).split(",").map(s => s.trim().toInt).toList
+  }
+
+  def resolveOperand(regexMatch: Match): Option[BigInt] = {
+    val value = regexMatch.group(4).toString
+    if (value == "old") None 
+    else Some(BigInt(value)) 
+  }
+
 
   def apply(monkeyStr: String): Monkey = {
     val pattern = """Monkey (\d+):\W+Starting items: ([^\n]+)\W+Operation: new = old ([+*-\\]) (\w+)\W+Test: divisible by (\d+)\W+If true: throw to monkey (\d+)\W+If false: throw to monkey (\d+)""".r
 
-    pattern.findFirstMatchIn(monkeyStr).map {
-      m =>
-        val monkeyNumber = BigInt(m.group(1))
-        val items = m.group(2).mkString.split(",").map(_.trim()).map(s => BigInt(s)).toList
-        val operand = if (m.group(4).toString == "old") None else Some(BigInt(m.group(4)))
-        val divider = BigInt(m.group(5))
-        val operation = resolveOperation(m.group(3).mkString)
-        val destinationIfTrue = m.group(6)
-        val destinationIfFalse = m.group(7)
-        val resolveNextMonkey = (num: BigInt) => {
-          if (num % divider == 0) BigInt(destinationIfTrue)
-          else BigInt(destinationIfFalse)
-        }
-        Monkey(monkeyNumber, items, operation(operand), resolveNextMonkey)
+    pattern.findFirstMatchIn(monkeyStr).map {m =>
+        val monkeyNumber = m.group(1).toInt
+        val items = resolveItems(m)
+        val operand = resolveOperand(m)
+        val operation = resolveOperation(m)
+        val resolveNextMonkey = createNextMonkeyResolver(m)
+        Monkey(monkeyNumber, items, operation(operand, _), resolveNextMonkey)
     }.get
   }
 }
@@ -55,7 +60,7 @@ object Solution extends App {
     for (j <- monkeys.indices) {
       val monkey = monkeys(j)
       for (item <- monkey.items) {
-        val updatedItem = monkey.operation(item)
+        val updatedItem = monkey.operation(item).toInt
         monkeys(j) = monkeys(j).copy(inspectedCount = monkeys(j).inspectedCount + 1)
         val nextMonkey = monkey.resolveNextMonkey(updatedItem).toInt
         val currentItems = monkeys(nextMonkey).items
