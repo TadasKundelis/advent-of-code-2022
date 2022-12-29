60b00b29422863cbba7f15a5222c91c8c09b47a5
Tadas Kundelis <kundelist@gmail.com>
2022-12-29 23:32:00 +0200

    refactor scala day 8

--- 08/solution.scala
+++ 08/solution.scala
@@ -4,23 +4,22 @@ import scala.math._
 object Solution extends App {
   val matrix = Source.fromFile("08/input.txt").getLines.toList.map(_.toList.map(_.toString.toInt))
 
-  val getMaxElement = (acc: List[Int], curr: Int) => acc :+ max(acc.last, curr)
-  val getMaxElementsFromLeft = (row: List[Int]) => row.drop(1).foldLeft(List(row.head))(getMaxElement)
+  type Matrix[T] = List[List[T]]
+
+  val getMaxElementsFromLeft = (row: List[Int]) => {
+    val rollingMax = row.scanLeft(-1) { case (acc, curr) => max(acc, curr) }.init
+    rollingMax.zip(row).map { case (max, curr) => max < curr }
+  }
 
   val maxFromLeft = matrix.map(getMaxElementsFromLeft)
   val maxFromRight = matrix.map(_.reverse).map(getMaxElementsFromLeft).map(_.reverse)
-  val maxFromTop = matrix.transpose.map(getMaxElementsFromLeft)
-  val maxFromBottom = matrix.transpose.map(_.reverse).map(getMaxElementsFromLeft).map(_.reverse)
+  val maxFromTop = matrix.transpose.map(getMaxElementsFromLeft).transpose
+  val maxFromBottom = matrix.transpose.map(_.reverse).map(getMaxElementsFromLeft).map(_.reverse).transpose
 
-  val res = matrix.zipWithIndex.map { case (row, rowIndex) =>
-    row.zipWithIndex.count { case (value, colIndex) =>
-      val seenFromLeft = if (colIndex > 0) maxFromLeft(rowIndex)(colIndex - 1) < value else true
-      val seenFromRight = if (colIndex < row.size - 1) maxFromRight(rowIndex)(colIndex + 1) < value else true
-      val seenFromTop = if (rowIndex > 0) maxFromTop(colIndex)(rowIndex - 1) < value else true
-      val seenFromBottom = if (rowIndex < matrix.size - 1) maxFromBottom(colIndex)(rowIndex + 1) < value else true
-      seenFromLeft || seenFromRight || seenFromTop || seenFromBottom
-    }
-  }.sum
+  def mergeMatrices(matrices: Matrix[Boolean]*): Matrix[Boolean] = matrices.reduce {
+    case (matrix1, matrix2) => matrix1.zip(matrix2).map((xs, ys) => xs.zip(ys).map((x, y) => x || y))
+  }
 
+  val res = mergeMatrices(maxFromLeft, maxFromRight, maxFromBottom, maxFromTop).flatten.count(_ == true)
   print(res == 1693)
 }
